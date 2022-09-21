164d99a17d420d1cb8d21baa47b47d940b205e0c
melissastan <melissastan91@gmail.com>
2022-09-20 23:34:15 -0400

    added comments

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const path = require('path');
 const uuid = require('./helper/uuid.js');
 
 const fs = require('fs');
-// const { notStrictEqual } = require('assert');
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Static files
 app.use(express.static('public'));
 
+// Homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -20,16 +22,16 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-//work of api
+//Work of api; getting notes from the server
 app.get('/api/notes', (req, res) => {
 
-  // res.sendFile(path.join(__dirname, '/public/notes.html'))
 
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
     } else {
       const getNote = JSON.parse(data);
+
       //stores the data to to getNote
       res.json(getNote)
     }
@@ -37,6 +39,7 @@ app.get('/api/notes', (req, res) => {
   console.log(`${req} request received to notes!`)
 });
 
+//Adding a note using POST HTTP method
 app.post('/api/notes', (req, res) => {
   console.log(`${req} request add to notes!`)
 
@@ -77,11 +80,11 @@ app.post('/api/notes', (req, res) => {
     res.status(500).json('Error in posting review');
   
   }
-});
-
+})
+;
+//Deleting a note
 app.delete('/api/notes/:id', (req, res) => {
   const { id } = req.params;
-  // const db = require('./db/db.json');
 
   // console.log(id);
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -90,10 +93,8 @@ app.delete('/api/notes/:id', (req, res) => {
     } else {
      const db = JSON.parse(data);
 
-      // const deletedNote = .filter(notStrictEqual=>notes!=req.params.id)
       const deletedNote = db.find(note => note.id === id);
       // console.log(deletedNote)        
-      // res.status(200).json(deletedNote);
 
       if (deletedNote){
         const newDB = db.filter(note => note.id !== id);
@@ -117,7 +118,6 @@ app.delete('/api/notes/:id', (req, res) => {
 
   })
 });
-
 
 
 app.listen(PORT, () => {
